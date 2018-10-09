cf51f1b580f0085a784bb3533ef11b256444578f
AshSeitz <ash.seitz94@gmail.com>
2018-10-09 11:23:52 -0400

    git commit -m "fixed word display, unfinished"

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -26,10 +26,10 @@ var characterNames = [
 ];
 
 var counter;
-var wrong;
-var correct;
-var wordElement = document.getElementById('word');
+var wrongGuess;
+var correctGuess;
 
+var wordElement = document.getElementById('word');
 var letterCountElement = document.getElementById("guesses")
 var lettersGuessedElement = document.getElementById("guesses")
 
@@ -43,7 +43,6 @@ var randomWord = characterNames[Math.floor(Math.random()* characterNames.length)
   
 //Function to render word
 
-  
     var answer = []
     for (let i = 0; i < randomWord.length; i++){
       answer[i] = "_";
@@ -51,43 +50,61 @@ var randomWord = characterNames[Math.floor(Math.random()* characterNames.length)
 
  console.log(randomWord)
  
- wordElement.append(answer)
- 
+ wordElement.innerHTML = answer
 
 //Use key events to listen for the letters that your players will type.
 document.onkeyup = function(event) {
 
-    
-    var chosenLetter = event.key.toLowerCase();
-    console.log(chosenLetter)
+     var chosenLetter = event.key.toLowerCase();
+     console.log(chosenLetter)
+    guesses.innerHTML = chosenLetter
 
-    console.log(randomWord.indexOf())
 
-   // if (chosenLetter === answer[x]) {
-       // for (var x = 0; x < randomWord.length; x++){
-         //   if (randomWord[x] === chosenLetter){
-           //     answer[x] = chosenLetter;
-      //      }
-    //    }
-  //  }
 
-    //else(counter <= 0 ); {
+     function updateGuesses(letter){
+         counter --;
+         letterCountElement.textContent = counter;
+     //if letter is not in the word
+         if(randomWord.indexOf(letter)=== -1){
+             wrongGuess.push(letter);
+             lettersGuessedElement.innerHTML = wrongGuess
+             console.log(letter)
+         }
+        
+    //if letter is in word replace letter into word
+    else { for (var i=0; randomWord.length; i++){
+         if (randomWord[i] === letter) {
+             correctGuess[i].replace(answer);
+         }
+     }
+         wordElement.innerHTML = correctGuess.join('')
+     }
+ }
+}
+//         //Win or Lose
+//         function checkWin(){
+//             if(correctGuess.indexOf(_)=== -1){
+//                 alert('You Won!')
+//             } 
+//             else if (counter === 0 ){
+//                 alert ('YOU LOST!');}
+//         }
+
+      
+//    // if (chosenLetter === answer[x]) {
+//        // for (var x = 0; x < randomWord.length; x++){
+//          //   if (randomWord[x] === chosenLetter){
+//            //     answer[x] = chosenLetter;
+//       //      }
+//     //    }
+//   //  }
+
+//     //else(counter <= 0 ); {
        
-        //location.reload(true)
-   // 
-;
-    }
- 
+//         //location.reload(true)
+//    // 
 
-    //Texts//
-   //document.getElementById("#word").textContent = randomWord;
-  // wordElement.textContent = answer;
-
-
-
-   
-
-//one letter
+//     }
  
 
 //Display the following on the page:
